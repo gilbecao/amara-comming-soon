3d10e3c82c987ef3b41227c4eae42cc9bfbefafd
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-16 10:02:28 +0200

    Add sustainability badge

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 import soon from './assets/soon.png';
 import logo from './assets/amarahotels-logo-vertical.svg';
+import beSustainableBadge from './assets/be-sustainable-badge.png';
 
 function App() {
   return (
     <div className="app">
+      
       <div className="app__container">
         <p>GET READY EVERYONE!</p>
         <h1>COMING</h1>
@@ -15,6 +17,9 @@ function App() {
         <div>
           <img src={logo} className="logo" alt="Amara hotels logo"/>
         </div>
+        <div className="sustainability-badge">
+        <img src={beSustainableBadge}  alt="Be sustainable badge"/>
+      </div>  
       </div>
     </div>
   );
